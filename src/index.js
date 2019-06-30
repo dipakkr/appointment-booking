3920a028a3a475185761914cfaa9efee991c5b95
Deepak <raj.deeepak@gmail.com>
2019-06-30 15:17:33 +0530

    updated screenshot

--- src/index.js
+++ src/index.js
@@ -11,7 +11,4 @@ import 'bootstrap/dist/js/bootstrap.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
